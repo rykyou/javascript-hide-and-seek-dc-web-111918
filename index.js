6dc7768d5dcca8dbedca409351cd4b604fbc9b08
rykyou <rkim1214@gmail.com>
2019-01-09 17:16:53 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n) {
+  const rankedListNumbers = document
+    .getElementById('app')
+    .querySelectorAll('ul.ranked-list li');
+
+  for (let i = 0; i < rankedListNumbers.length; i++) {
+    rankedListNumbers[i].innerHTML =
+      (parseInt(rankedListNumbers[i].innerHTML, 10) + n).toString();
+  }
+}
+
+function deepestChild() {
+  let node = document.getElementById('grand-node');
+  let nextNode = node.firstElementChild;
+
+  while (nextNode) {
+    node = nextNode;
+    nextNode = node.firstElementChild;
+  }
+
+  return node;
+}
